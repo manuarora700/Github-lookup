1095b3d103de84af307cab351554c82e069ff369
Manu Arora <mannuarora700@gmail.com>
2020-03-06 14:14:38 +0530

    Fetching data from github api

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from "react";
+import Navbar from "./components/layout/Navbar";
+import UserItem from "./components/users/UserItem";
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Hello from React</h1>
-        <p>Working</p>
+        <Navbar title="Github Lookup" icon="fab fa-github" />
+        <UserItem />
       </div>
     );
   }

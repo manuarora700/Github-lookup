261ee46263912747854fea4c89a64f1f5a417940
Manu Arora <mannuarora700@gmail.com>
2020-03-06 15:19:41 +0530

    functional components // custom github api id // axios

--- src/App.js
+++ src/App.js
@@ -1,15 +1,30 @@
 import React, { Component } from "react";
 import Navbar from "./components/layout/Navbar";
 import Users from "./components/users/Users";
+import axios from "axios";
 import "./App.css";
 
 class App extends Component {
+  state = {
+    users: [],
+    loading: false
+  };
+  async componentDidMount() {
+    this.setState({ loading: true });
+
+    const res = await axios.get(
+      `https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_IT}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET})`
+    );
+
+    this.setState({ users: res.data, loading: false });
+    console.log(res.data);
+  }
   render() {
     return (
       <div className="App">
         <Navbar title="Github Lookup" icon="fab fa-github" />
         <div className="container">
-          <Users />
+          <Users loading={this.state.loading} users={this.state.users} />
         </div>
       </div>
     );

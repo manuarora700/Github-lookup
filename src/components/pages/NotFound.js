ec108164a03d9b3adcc84dec981856baf9cdc917
Manu Arora <mannuarora700@gmail.com>
2020-03-06 22:47:18 +0530

    Ready to deploy // UI makeover required

---
+++ src/components/pages/NotFound.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const NotFound = () => {
+  return (
+    <div>
+      <h1>Not Found</h1>
+      <p className="lead">Page you're looking for does not exist</p>
+    </div>
+  );
+};
+
+export default NotFound;
